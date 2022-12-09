6df72c61187ed8f51d414f4f365ce80f637e8cd1
Damian <damian.gawrys@gmail.com>
2022-12-09 13:34:58 +0100

    created tools for each task

--- script.js
+++ script.js
@@ -25,13 +25,37 @@ const addNewToDo = () => {
     if(todoInput.value !== '') {
         newToDo = document.createElement('li')
         newToDo.textContent = todoInput.value
+        createToolsArea()
+        
         ulList.append(newToDo)
+
 
         todoInput.value = ''
         errorInfo.textContent = ''
     } else {
         errorInfo.textContent = 'Wpisz treść zadania'
     }
+}
+
+
+const createToolsArea = () => {
+    const tools = document.createElement('div')
+    tools.classList.add('tools')
+    newToDo.append(tools)
+    
+    const completeBtn = document.createElement('button')
+    completeBtn.classList.add('complete')
+    completeBtn.innerHTML = '<i class="fas fa-check"></i>'
+
+    const editBtn = document.createElement('button')
+    editBtn.classList.add('edit')
+    editBtn.textContent = 'EDIT'
+
+    const deleteBtn = document.createElement('button')
+    deleteBtn.classList.add('delete')
+    deleteBtn.innerHTML = '<i class="fas fa-check"></i>'
+
+    tools.append(completeBtn, editBtn, deleteBtn)
 }
 
 
